ec9b76c939398835786030d494e0e7b21e8d5cd5
Raven <iceshield18363@gmail.com>
2024-10-23 19:05:49 +0300

    Horse cross js + json

--- src/commands/horseStuff/horse.js
+++ src/commands/horseStuff/horse.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 const baseCombos = require("../../functions/datafiles/horseColors.json");
-const breeds = require("../../functions/datafiles/breeds.json")
+const breeds = require("../../functions/datafiles/breeds.json");
+const horseCross = require("../../functions/datafiles/horseCross.json");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,7 +13,7 @@ module.exports = {
         .setDescription("Base color results.")
         .addStringOption((option) =>
           option
-            .setName("firstcolor")
+            .setName("color1")
             .setDescription("First parent base color.")
             .setRequired(true)
             .addChoices(
@@ -39,7 +40,7 @@ module.exports = {
         )
         .addStringOption((option) =>
           option
-            .setName("secondcolor")
+            .setName("color2")
             .setDescription("Second parent base color.")
             .setRequired(true)
             .addChoices(
@@ -69,65 +70,70 @@ module.exports = {
       subcommand
         .setName("breed")
         .setDescription("ä")
-        .addStringOption((option) =>
-          option
-            .setName("breed1")
-            .setDescription("ö")
-            .setRequired(true)
-            .setAutocomplete(true)
-        )
-        .addStringOption((option) =>
-          option
-            .setName("breed2")
-            .setDescription("ü")
-            .setRequired(true)
-            .setAutocomplete(true)
-        )
+        .addStringOption((option) => option.setName("breed1").setDescription("ö").setRequired(true).setAutocomplete(true))
+        .addStringOption((option) => option.setName("breed2").setDescription("ü").setRequired(true).setAutocomplete(true))
     ),
-    async autocomplete(interaction, client) {
-      const focusedValue = interaction.options.getFocused();
-      var choices = breeds;
-      var filtered = choices.filter((choice) => choice.name.toLowerCase().includes(focusedValue.toLowerCase()));
-      const max = 5;
-      filtered = filtered.length > max ? filtered.slice(0, max) : filtered;
-      await interaction.respond(filtered);
-    },
+  async autocomplete(interaction, client) {
+    const focusedValue = interaction.options.getFocused();
+    var choices = breeds;
+    var filtered = choices.filter((choice) => choice.name.toLowerCase().includes(focusedValue.toLowerCase()));
+    const max = 5;
+    filtered = filtered.length > max ? filtered.slice(0, max) : filtered;
+    await interaction.respond(filtered);
+  },
 
   async execute(interaction, client) {
     await interaction.deferReply({ fetchReply: true });
+    let result = null;
 
     switch (interaction.options.getSubcommand(false)) {
       // color subcommand
       case "color":
-        const firstchoice = interaction.options.getString("firstcolor");
-        const secondchoice = interaction.options.getString("secondcolor");
-        let result = null;
+        const firstChoice = interaction.options.getString("color1");
+        const secondChoice = interaction.options.getString("color2");
         // loop to find the 4 results from base breeding
         for (const combo of baseCombos) {
           if (
-            (combo.baseColor[0] === firstchoice && combo.baseColor[1] === secondchoice) ||
-            (combo.baseColor[1] === firstchoice && combo.baseColor[0] === secondchoice)
+            (combo.baseColor[0] === firstChoice && combo.baseColor[1] === secondChoice) ||
+            (combo.baseColor[1] === firstChoice && combo.baseColor[0] === secondChoice)
           ) {
-            result = combo.baseResult;
+            let sames = [];
+            result = [];
+            for (const color of combo.baseResult) {
+              if (sames.includes(color)) continue;
+              let count = combo.baseResult.filter((c) => c === color).length;
+              result.push(`${count * 25}% ${color}`);
+              sames.push(color);
+            }
+            result = result.join(", ");
             break;
           }
         }
-        if (result === null) return await interaction.editReply("How the fuck did you get this? Probs tag @ dev");
+        if (!result) return await interaction.editReply("How the fuck did you get this? Probs tag @ dev");
 
-        let newMessage = `Bases: ${firstchoice} + ${secondchoice}\nResults: `;
-        let sames = [];
-        for (const color of result) {
-          if (sames.includes(color)) continue;
-          let count = result.filter((c) => c === color).length;
-          newMessage += `${count * 25}% ${color}, `;
-          sames.push(color);
-        }
-        newMessage = newMessage.slice(0, -2);
+        let newMessage = `Bases: ${firstChoice} + ${secondChoice}\nResults: ${result}`;
+
         return await interaction.editReply({ content: newMessage });
 
-      // breed subcommand
+      /// breed subcommand
       case "breed":
-        return await interaction.editReply(`*[wip]*`);
+        const firstBreed = interaction.options.getString("breed1");
+        const secondBreed = interaction.options.getString("breed2");
+        // loop to find the 4 results from base breeding
+        for (const combo of horseCross) {
+          if (
+            (combo.parents[0] === firstBreed && combo.parents[1] === secondBreed) ||
+            (combo.parents[1] === firstBreed && combo.parents[0] === secondBreed)
+          ) {
+            result = combo.foal.join(", ");
+            break;
+          }
+        }
+        if (!result) result = `25% ${firstBreed}, 25% ${secondBreed}, 50% grade`;
+
+        let breedMessage = `Parents: ${firstBreed} + ${secondBreed}\nFoal: ${result}`;
+
+        return await interaction.editReply({ content: breedMessage });
       default:
         return await interaction.editReply("error message I'm so confused where am I");
     }
